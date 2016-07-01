93118b038dd1b77f6a4acefc426ec762e31860ff
yuanyuan5 <yuanyuan5@kingsoft.com>
2016-07-01 10:54:33 +0800

    添加了可供用户选择年月的选择框

--- js/datepicker.js
+++ js/datepicker.js
@@ -5,12 +5,10 @@
 
 	// 默认参数
 	var defaults = { 
-		dateWidth: 80, 				// 每个日期的小框的宽度
-		dateHeight: 50, 			// 每个日期的小框的高度
-		theme: 'primary',			// 整体的主题 primary|success|info|warning|danger
-		left: 0, 					// 日历相对于input框的左边距离(不可选)
-		top: 0, 
-		separator: '-' 					// 日历相对于input框的上边距离(不可选)
+		dateWidth: 60, 				// 每个日期的小框的宽度，不能小于60
+		dateHeight: 40, 			// 每个日期的小框的高度，不能小于30
+		theme: 'primary',
+		separator: '-' 
 	};
 
 	function init(options){
@@ -21,6 +19,8 @@
 	// 创建一个日历整体框架
 	function createMainEle(options){
 		this.options = $.extend({}, defaults, options);
+		this.options.dateWidth = (this.options.dateWidth < 60) ? 60 : this.options.dateWidth; 
+		this.options.dateHeight = (this.options.dateHeight < 30) ? 30 : this.options.dateHeight;
 		this.mainEle = $('<div id="datePicker"></div>')
 					.addClass('dp-main-ele-' + this.options.theme)
 					.css('width', this.options.dateWidth * 7 + 'px')
@@ -28,20 +28,21 @@
 					.css('left', this.options.left + 'px')
 					.css('top', this.options.top + 'px')
 					.appendTo('body');
-		var p = $('<p></p>')
-				.addClass('dp-header-' + this.options.theme)
+		var titleDiv = $('<div></div>')
+				.addClass('dp-titlediv-' + this.options.theme)
 				.appendTo(this.mainEle);
-		var title = $('<strong></strong>').addClass('dp-title-text').appendTo(p);
 		var _this = this;
 		var btnLeft = $('<strong><-</strong>')
-					.addClass('dp-btn-left')
-					.appendTo(p)
-					.on('click', function(){
-						_this.prevMonth();
-					});
+			.addClass('dp-btn-left')
+			.appendTo(titleDiv)
+			.on('click', function(){
+				_this.prevMonth();
+			});
+		var selectDiv = $('<div></div>').addClass('dp-selectdiv').appendTo(titleDiv);
+		createSelect.call(this, selectDiv);
 		var btnRight = $('<strong>-></strong>')
 					.addClass('dp-btn-right')
-					.appendTo(p)
+					.appendTo(titleDiv)
 					.on('click', function(){
 						_this.nextMonth();
 					});
@@ -70,6 +71,28 @@
 				}
 			}
 		});
+		// 点击选择select的时候触发change事件
+		// 而在点击日期的时候就不会触发change事件
+		$('.dp-select-year').on('click', function(){
+			$(this).on('change', function(){
+				var _selectyear = $('.dp-select-year option:selected').html();
+				var _selectmonth = $('.dp-select-month option:selected').html();
+				var date = _this.date;
+				date.setYear(_selectyear);
+				date.setMonth(_selectmonth);
+				_this.gotoDate(date);
+			});
+		});
+		$('.dp-select-month').on('click', function(){
+			$(this).on('change', function(){
+				var _selectyear = $('.dp-select-year option:selected').html();
+				var _selectmonth = $('.dp-select-month option:selected').html();
+				var date = _this.date;
+				date.setYear(_selectyear);
+				date.setMonth(_selectmonth - 1);
+				_this.gotoDate(date);
+			});
+		});	
 	}
 
 	// 创建日历中的所有span，包括星期和日期框
@@ -82,20 +105,48 @@
 		return spanEle;
 	}
 
+	// 创建日历顶部的选择框
+	function createSelect(selectDiv){
+		for (var i = 0; i < 2; i++){
+			var select = $('<select></select>').addClass('dp-select-' + this.selectClass[i]).appendTo(selectDiv);
+			var strong = $('<strong></strong>').addClass('dp-select-strong').html(this.options.separator).appendTo(selectDiv);
+		}
+		year();
+		month();
+	}
+
 	// 构造日历插件函数
 	function DatePicker($ele, options){
 		this.$ele = $ele;
 		this.judge(options);
 	}
 
+	// 年份选择
+	// 年份最好从最近的年份开始，所以使用的是i--循环
+	function year(){
+		var year = '';
+		var today = new Date().getFullYear();
+		for(var i = today+2; i >= 1900; i--){
+			year += '<option value="'+i+'">'+i+'</option>';
+		}
+		$(year).appendTo('.dp-select-year');
+	}
+
+	// 月份选择
+	function month(){
+		var month = '';
+		for(var i = 1; i < 13; i++){
+			month += '<option value="'+i+'">'+i+'</option>';
+		}
+		$(month).appendTo('.dp-select-month');
+	}
+
 	DatePicker.prototype = {
 		date: new Date(),
-		days: ['日', '一', '二', '三', '四', '五', '六'],	// 星期
+		selectClass: ['year', 'month', 'date'],
+		days: ['日', '一', '二', '三', '四', '五', '六'],// 星期
 		// 渲染日期元素到框架中
 		renderByDate: function(date){
-			var _titleText = date.getFullYear() + this.options.separator + (date.getMonth() + 1) + this.options.separator + date.getDate() + this.options.separator + '周' + this.days[date.getDay()];
-			$('.dp-title-text').html(_titleText);
-			this.$ele.val(_titleText);
 			var _date = new Date(date);
 			_date.setDate(1); // 找到该月的一号
 			_date.setDate(_date.getDate() - _date.getDay()); // 找到该页的第一个日期是上个月的几号,_date现在是这一页的第一个日期
@@ -117,7 +168,26 @@
 				}
 				_date.setDate(_date.getDate() + 1);
 			}
+			this.showDate(date);
 		}, 
+		showDate: function(date){
+			var _titleText = date.getFullYear() + this.options.separator + (date.getMonth() + 1) + this.options.separator + date.getDate() + this.options.separator + '周' + this.days[date.getDay()];
+			var today = new Date();
+			var allyears = today.getFullYear() - 1900;
+			$('.dp-select-year option').attr('selected', false);
+			$('.dp-select-month option').attr('selected', false);
+			for (var i = 0; i < allyears; i++){
+				if($('.dp-select-year option').eq(i).html() == date.getFullYear()) {
+					$('.dp-select-year option').eq(i).attr('selected', 'selected');
+				}
+			}
+			for (var i = 0; i <= 12; i++){
+				if($('.dp-select-month option').eq(i).html() == date.getMonth()+1){
+					$('.dp-select-month option').eq(i).attr('selected', 'selected');
+				}
+			}
+			this.$ele.val(_titleText);
+		},
 		prevMonth: function(){
 			var _date = new Date(this.date);
 			_date.setMonth(_date.getMonth() - 1);
@@ -138,9 +208,7 @@
 				var _selectedIndex = _allSpan.index(this.selectedEle);
 				var _temp = _allSpan.get(_selectedIndex + date.getDate() - this.date.getDate());
 				this.selectedEle = $(_temp).addClass('dp-active-' + this.options.theme);
-				var _dateText = date.getFullYear() + this.options.separator + (date.getMonth() + 1) + this.options.separator + date.getDate() + this.options.separator + '周' + this.days[date.getDay()];
-				$('.dp-title-text').html(_dateText);
-				this.$ele.val(_dateText);
+				this.showDate(date);
 			}else {
 				this.renderByDate(date);
 			}
@@ -161,9 +229,11 @@
 					}else{
 						options = position;
 					}
+					_this.$ele.attr('disabled', 'disabled');
 					init.call(_this, options);
 				}else if($('#datePicker')[0] && !/^dp-/.test($(e.target).attr('class'))){
 					$('#datePicker').remove();
+					_this.$ele.attr('disabled', false);
 				}
 			});
 		}
